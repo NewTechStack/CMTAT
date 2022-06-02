78f7b73070183adb275c81915ac057b1a753bf8d
SCcagg5 <sccagg5@gmail.com>
2022-06-02 12:33:35 +0200

    add

--- contracts/mocks/MinimalForwarderMock.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.8.2;
-
-import "../../openzeppelin-contracts-upgradeable/contracts/metatx/MinimalForwarderUpgradeable.sol";
-
-contract MinimalForwarderMock is MinimalForwarderUpgradeable {
-  function initialize() public initializer {
-    __MinimalForwarder_init();
-  }
-}
